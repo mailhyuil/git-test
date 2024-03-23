e565a3cb8439b3a4baf7dd8bcafc13569783c1a7
mailhyuil <mailhyuil@gmail.com>
2024-03-23 14:45:12 +0900

    chore:update test

---
+++ main.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function main() {
+  console.log(add(1, 2));
+  console.log(subtract(1, 2));
+}
+
+main();
