1296879cc373308df8fe61046e7db7214f3402fb
mailhyuil <mailhyuil@gmail.com>
2024-03-25 21:39:20 +0900

    chore:update test

--- main.go
+++ main.go
@@ -1,31 +1,6 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
-)
-
 var baseUrl = "https://www.fmkorea.com/index.php?mid=best&page=5"
 
 func main() {
-	getPages()
-}
-
-func getPages() {
-	res, err := http.Get(baseUrl)
-	if err != nil || res.StatusCode > 400 {
-		log.Fatalln("Request failed with status code", res.StatusCode)
-	}
-	
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	if err != nil {
-		log.Fatalln("Error loading HTTP response body. ", err)
-	}
-	defer res.Body.Close()
-	
-	doc.Find(".bd_pg").Each(func(i int, s *goquery.Selection) {
-		log.Println(s.Html())
-	})
 }
